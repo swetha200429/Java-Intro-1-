44a0fe321a522bd0adb69fbcdc9c5275d8b65cc6
swetha200429 <swetha200429@gmail.com>
2026-03-05 09:53:21 +0530

    Create FinancialLossAnalyzer.java

---
+++ FinancialLossAnalyzer.java
@@ -0,0 +1,12 @@
+ class FinanacialLossAnalyzer{
+  public static void main(String[] args){
+    double january=-47.786;
+    double february=-48.236;
+    double march=-20.773;
+    double janLoss=Math.abs(january);
+    double febLoss=Math.abs(february);
+    System.out.println("January Loss:"+janLoss);
+    System.out.println("February Loss:"+febLoss);
+    System.out.println("March Loss:"+march);
+  }
+}
