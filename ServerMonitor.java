f6c236ff99b294fff3b4dd7dc28a9e00d31d35ef
swetha200429 <swetha200429@gmail.com>
2026-03-06 19:21:29 +0530

    Create ServerMonitor.java

---
+++ ServerMonitor.java
@@ -0,0 +1,9 @@
+class ServerMonitor{
+  public static void main(String[] args){
+    double temperatur=80.4;
+    if(temperature>75){
+      System.out.println("Warning! Server Overheating!");
+    }
+    System.out.println("Current Temperature: " + temperature + "°C");
+  }
+}
