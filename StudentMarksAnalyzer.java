54bd28d79114eab22eb0f42a60f9a43a8f59a6ad
swetha200429 <swetha200429@gmail.com>
2026-03-07 11:40:20 +0530

    Create StudentMarksAnalyzer.java

---
+++ StudentMarksAnalyzer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class StudentMarksAnalyzer{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int marks;
+    int total=0;
+    int count=0;
+    int highest=0;
+    System.out.println("Enter student marks(-1 to stop):");
+    marks=sc.nextInt();
+    while(marks!=-1){
+      total+=marks;
+      total=total+marks;
+    }
+      
