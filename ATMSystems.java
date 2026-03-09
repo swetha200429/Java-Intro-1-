fdf2ab8545deb2477c62e8e1035abfa6dd8bd88b
swetha200429 <swetha200429@gmail.com>
2026-03-09 11:55:13 +0530

    Update ATMSystems.java

--- ATMSystems.java
+++ ATMSystems.java
@@ -1,29 +1,49 @@
 import java.util.Scanner;
 class ATMSystems{
-  public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     int choice;
-    double balance=10000;
-    double amount;
-    do{
-      System.out.println("-----ATM Menu ------");
-      System.out.println("1.Withdraw");
-      System.out.println("2.Check Balance");
-      System.out.println("3.Exit");
-      System.out.println("Enter choice:");
-      choice=sc.nextInt();
-      switch(choice){
-          case1:
-          System.out.println("enter amount:");
-          amount=sc.nextDouble();
-          if(amount<=balance){
-            balance-=amount;
-            System.out.println("Withdrawal successful!");
-          }
-          else{
-            system.out.println("Insufficient balance!");
-          }
-          break;
-          case2:
-          System.out.printlnm
-          caes:
+    double balance = 10000;
+        double amount;
+
+        do {
+
+            System.out.println("\n------ ATM Menu ------");
+            System.out.println("1. Withdraw");
+            System.out.println("2. Check Balance");
+            System.out.println("3. Exit");
+
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter amount: ");
+                    amount = sc.nextDouble();
+
+                    if (amount <= balance) {
+                        balance -= amount;
+                        System.out.println("Withdrawal successful!");
+                    } else {
+                        System.out.println("Insufficient balance!");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Current Balance: ₹" + balance);
+                    break;
+
+                case 3:
+                    System.out.println("Thank you for using ATM!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+        } while (choice != 3);
+
+        sc.close();
+    }
+}
